27c504a4e102bf584d37b83a375ce67c976f4ed6
mateszabo02 <mateszabo02@stud.uni-obuda.hu>
2025-11-14 12:39:13 +0100

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Receptkonyv_MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Receptkonyv_MAUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Receptkonyv_MAUI.Pages")]
